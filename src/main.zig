48ceb136dedb9eaa52439a75654803796d4d04cf
Emil Lerch <emil@lerch.org>
2021-10-03 12:01:06 -0700

    remove build comments from main

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,6 @@
 const std = @import("std");
 const lambda = @import("lambda.zig");
 
-// zig package -Drelease-safe does the following:
-//
-// zig build -Drelease-safe
-// strip zig-out/bin/bootstrap # should this be stripped?
-// zip -j9 zig-out/bin/function.zip zig-out/bin/bootstrap
-//
-// zig deploy will also do something like:
-//
-// aws lambda update-function-code --function-name zig-test --zip-file fileb://function.zip
 pub fn main() anyerror!void {
     try lambda.run(handler);
 }
