ef5b793882c60ff3d9f3e088fc47758e8ec5a2bc
Emil Lerch <emil@lerch.org>
2024-06-08 10:01:40 -0700

    upgrade to zig 0.13.0

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) !void {
         .name = "lambda-zig",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/lambda.zig" },
+        .root_source_file = b.path("src/lambda.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -38,7 +38,7 @@ pub fn build(b: *std.Build) !void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/lambda.zig" },
+        .root_source_file = b.path("src/lambda.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -53,7 +53,7 @@ pub fn build(b: *std.Build) !void {
 
     const exe = b.addExecutable(.{
         .name = "custom",
-        .root_source_file = .{ .path = "src/sample-main.zig" },
+        .root_source_file = b.path("src/sample-main.zig"),
         .target = target,
         .optimize = optimize,
     });
